06b71ffce490639a320c18bc7e41327f1a8bbbc2
cyrus-op <76032422+cyrus-op@users.noreply.github.com>
2021-01-27 06:11:20 -0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,18 @@
+from torbot.sample_config import Config
+
+#read readme too before filling these stuffs
+
+class Config(Config):
+	TG_BOT_TOKEN= "1442173461:AAH82dvwnid7d1nC7jVJ9-1sQARKEgA3W9I"
+	APP_ID = 2526751
+	API_HASH = "9d95cd9e9bede6332e1da19276d9ad63"
+	AUTH_CHANNEL = [-1001468189721, 1197580900]
+	INDEX_LINK= ""
+	LEECH_COMMAND = "leech"
+	YTDL_COMMAND = "ytdl"
+	DESTINATION_FOLDER = "TorrentLeech-Gdrive"
+	GLEECH_COMMAND = "gleech"
+	TELEGRAM_LEECH_COMMAND_G = "tleech"
+	PYTDL_COMMAND_G = "pytdl"
+	CLONE_COMMAND_G = "gclone"
+	RCLONE_CONFIG = ""
